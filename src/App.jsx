3423960ffeb84dbb563e342dcfbe63d9936353f0
Rohan Singh Jaglan <rohan.singh@kalvium.community>
2025-03-19 12:08:13 +0530

    PA1 Done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DestinationContainer from "./components/DestinationContainer";
+
+const App = () => {
+  return (
+    <div>
+      <DestinationContainer />
+    </div>
+  );
+};
+
+export default App;
